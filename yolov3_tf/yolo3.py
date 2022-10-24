4b8586279e1d38a16047350e8bd15ea463af81c4
aka-vm <vineetmahajan2000@gmail.com>
2022-10-24 06:59:28 +0530

    minor improvements and bug fix

--- yolov3_tf/yolo3.py
+++ yolov3_tf/yolo3.py
@@ -1,9 +1,10 @@
+from operator import mod
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential
-from keras.layers import Layer, Conv2D, BatchNormalization, LeakyReLU, UpSampling2D, Add, Concatenate
+from keras.layers import Layer, Conv2D, UpSampling2D, Add, Concatenate
 
-from  .utils import PASCAL_ANCHORS
+from utils import PASCAL_ANCHORS
 # Conv2d layer with optional batch normalization and leaky relu
 
 
@@ -11,7 +12,7 @@ class ConvBlock(Conv2D):
 
     def __init__(self, output_filters, kernel_size, normalize=True, downsample=False, **kwargs):
         strides, padding = (2, 'same') if downsample else (1, 'same')
-        super(ConvBlock, self).__init__(output_filters, kernel_size, strides=strides, padding=padding, **kwargs)
+        super().__init__(output_filters, kernel_size, strides=strides, padding=padding, **kwargs)
         self.normalize = normalize
         if normalize:
             self.use_bias = False
@@ -30,14 +31,14 @@ class ConvBlock(Conv2D):
 class ResidualBlock(Layer):
 
     def __init__(self, filters:int, repetes:int = 1, **kwargs):
-        super(ResidualBlock, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.filters = filters
         self.repetes = repetes
         self.layes = [
             Sequential([
                 ConvBlock(filters//2, 1),
                 ConvBlock(filters, 3)
-            ])
+            ]) for _ in range(repetes)
         ]
 
     def call(self, inputs):
@@ -50,7 +51,7 @@ class ResidualBlock(Layer):
 class ConvPassBlock(Layer):
 
     def __init__(self, filters, **kwargs):
-        super(ConvPassBlock, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.layers = Sequential([
             ConvBlock(filters, 1),
             ConvBlock(filters*2, 3),
@@ -66,7 +67,7 @@ class ConvPassBlock(Layer):
 class OutputBlock(Layer):
 
     def __init__(self, filters:int, num_classes:int, num_anchors:int=3, **kwargs):
-        super(OutputBlock, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.num_anchors = num_anchors
         self.num_classes = num_classes
         self.layer = Sequential([
@@ -91,72 +92,94 @@ class YOLOv3(Model):
         anchors=None,
         **kwargs
     ):
+        super().__init__(**kwargs)
+
         self.num_classes = num_classes
         self.initial_filters = initial_filters
         self.anchors = anchors or PASCAL_ANCHORS
 
         input_layer = tf.keras.Input(shape=input_shape)
-        outputs = self._create_model_layers(input_layer)
-        super(YOLOv3, self).__init__(**kwargs, inputs=input_layer, outputs=outputs)
+        self._model_layers = self._create_model_layers()
 
-    def _create_model_layers(self, input_layer):
-        FILTELS = self.initial_filters
-        routes = []
+    def call(self, inputs):
         outputs = []
+        route_layers = []
+        x = inputs
 
-        # 1st block (416, 416, 3) -> (416, 416, 32)
-        x = ConvBlock(FILTELS, 3)(input_layer)
-        # 2st block (416, 416, 32) -> (208, 208, 64)
-        x = ConvBlock(FILTELS*2, 3, downsample=True)(x)
-        x = ResidualBlock(FILTELS*2)(x)
-        # 3st block (208, 208, 64) -> (104, 104, 128)
-        x = ConvBlock(FILTELS*4, 3, downsample=True)(x)
-        x = ResidualBlock(FILTELS*4, repetes=2)(x)
-        # 4st block (104, 104, 128) -> (52, 52, 256)
-        x = ConvBlock(FILTELS*8, 3, downsample=True)(x)
-        x = ResidualBlock(FILTELS*8, repetes=8)(x)
-        routes.append(x)
-        # 5st block (52, 52, 256) -> (26, 26, 512)
-        x = ConvBlock(FILTELS*16, 3, downsample=True)(x)
-        x = ResidualBlock(FILTELS*16, repetes=8)(x)
-        routes.append(x)
-        # 6st block (26, 26, 512) -> (13, 13, 1024)
-        x = ConvBlock(FILTELS*32, 3, downsample=True)(x)
-        x = ResidualBlock(FILTELS*32, repetes=4)(x)
-        # Darknet53 Ends
+        for layer_name, layer in self._model_layers.items():
+            if isinstance(layer, OutputBlock):
+                outputs.append(layer(x))
+                continue
 
-        x = ConvPassBlock(FILTELS*16)(x)
-        # Output 1 (13, 13, 3, 5+n_classes)
-        outputs.append(OutputBlock(FILTELS*32, self.num_classes)(x))
-
-        # 7st block (13, 13, 512) -> (26, 26, 512)
-        x = ConvBlock(FILTELS*8, 1)(x)
-        x = UpSampling2D(2)(x)
-        x = Concatenate()([x, routes.pop(-1)])
-        x = ConvPassBlock(FILTELS*8)(x)
-        # Output 2 (26, 26, 3, 5+n_classes)
-        outputs.append(OutputBlock(FILTELS*16, self.num_classes)(x))
-
-        # 8st block (26, 26, 256) -> (52, 52, 256)
-        x = ConvBlock(FILTELS*4, 1)(x)
-        x = UpSampling2D(2)(x)
-        x = Concatenate()([x, routes.pop(-1)])
-        x = ConvPassBlock(FILTELS*4)(x)
-        # Output 3 (52, 52, 3, 5+n_classes)
-        outputs.append(OutputBlock(FILTELS*8, self.num_classes)(x))
+            x = layer(x)
+            if isinstance(layer, UpSampling2D):
+                x = Concatenate()([x, route_layers.pop()])
+            elif isinstance(layer, ResidualBlock) and layer.repetes == 8:
+                route_layers.append(x)
 
         return outputs
 
 
+    def _create_model_layers(self):
+        FILTELS = self.initial_filters
+        layers = dict(
+            conv_1              = ConvBlock(FILTELS, 3, name='conv_1'),
+            downsample_conv_1   = ConvBlock(FILTELS*2, 3, downsample=True, name='downsample_conv_1'),
+            residual_block_1    = ResidualBlock(FILTELS*2, name='residual_block_1'),
+            downsample_conv_2   = ConvBlock(FILTELS*4, 3, downsample=True, name='downsample_conv_2'),
+            residual_block_2    = ResidualBlock(FILTELS*4, repetes=2, name='residual_block_2'),
+            downsample_conv_3   = ConvBlock(FILTELS*8, 3, downsample=True, name='downsample_conv_3'),
+            residual_block_3    = ResidualBlock(FILTELS*8, repetes=8, name='residual_block_3'),                   # Route 2
+            downsample_conv_4   = ConvBlock(FILTELS*16, 3, downsample=True, name='downsample_conv_4'),
+            residual_block_4    = ResidualBlock(FILTELS*16, repetes=8, name='residual_block_4'),                   # Route 1
+            downsample_conv_5   = ConvBlock(FILTELS*32, 3, downsample=True, name='downsample_conv_5'),
+            residual_block_5    = ResidualBlock(FILTELS*32, repetes=4, name='residual_block_5'),
+            # Darknet53 ends here
+
+            conv_pass_1     = ConvPassBlock(FILTELS*16, name='conv_pass_1'),
+            output_1        = OutputBlock(FILTELS*32, self.num_classes, name='output_1'),
+
+            conv_2          = ConvBlock(FILTELS*8, 1, name='conv_2'),
+            upsample_1      = UpSampling2D(2, name='upsample_1'),
+            # concat_1
+            conv_pass_2     = ConvPassBlock(FILTELS*8, name='conv_pass_2'),
+            output_2        = OutputBlock(FILTELS*16, self.num_classes, name='output_2'),
+
+            conv_3          = ConvBlock(FILTELS*4, 1, name='conv_3'),
+            upsample_2      = UpSampling2D(2, name='upsample_2'),
+            # concat_2
+            conv_pass_3     = ConvPassBlock(FILTELS*4, name='conv_pass_3'),
+            output_3        = OutputBlock(FILTELS*8, self.num_classes, name='output_3'),
+        )
+
+        return layers
+
+
 if __name__ == "__main__":
-    model = YOLOv3(input_shape=(416, 416, 3), num_classes=80, name="YOLOv3")
+    num_classes = 20
+    IMAGE_SIZE = 416
+    initial_filters = 32
+    model = YOLOv3(input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), num_classes=num_classes, initial_filters=initial_filters, name='YOLOv3')
+
+    inputs = tf.random.normal((2, IMAGE_SIZE, IMAGE_SIZE, 3))
+    # op = model(inputs)
+
+    # assert op[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, num_classes + 5)
+    # assert op[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, num_classes + 5)
+    # assert op[2].shape == (2, 3, IMAGE_SIZE//8, IMAGE_SIZE//8, num_classes + 5)
+
+    # model.summary()
+
+    model.build(input_shape=(None, IMAGE_SIZE, IMAGE_SIZE, 3))
     model.summary()
-    plot_location = "./YOLOv3-tf.png"
+
+    plot_name = './YOLOv3-tf.png'
     tf.keras.utils.plot_model(
-                    model,
-                    to_file=plot_location,
-                    show_shapes=True,
-                    show_layer_names=False,
-                    expand_nested=False,
-                    dpi=256
-    )
+        model,
+        plot_name,
+        show_shapes=True,
+        show_layer_names=True,
+        rankdir='TB',
+        expand_nested=True,
+        dpi=256
+        )
