2da5b643a72a96ef6d2490fc2a52fcbfb6543c21
aka-vm <vineetmahajan2000@gmail.com>
2023-04-29 13:30:22 +0530

    yolo-tiny base done

--- yolov3_torch/yolo3.py
+++ yolov3_torch/yolo3.py
@@ -8,14 +8,14 @@ from torchinfo import summary
 
 import math
 
-# from .utils import (
-#     anchor_box_convert
-# )
-# from .config import (
-#     ANCHORS,
-#     NUM_CLASSES,
-#     CLASS_LABELS
-# )
+from utils import (
+    anchor_box_convert
+)
+from config import (
+    ANCHORS,
+    NUM_CLASSES,
+    CLASS_LABELS
+)
 
 
 class ConvBlock(nn.Module):
@@ -66,6 +66,7 @@ class YOLOv3(nn.Module):
         self._initial_filters = initial_filters
         self._base_output_scale = base_output_scale
         self._anchors = anchors
+
         self.layers = self._create_model()
 
     def forward(self, x):
@@ -86,7 +87,7 @@ class YOLOv3(nn.Module):
 
         return outputs
 
-    def summary(self):
+    def summary(self, verbose: int=1, **kwargs):
         """
         Prints the summary of the model.
         """
@@ -95,7 +96,8 @@ class YOLOv3(nn.Module):
             input_size=self._input_shape,
             batch_dim=0,
             col_names = ("input_size", "output_size", "num_params", "kernel_size"),
-            verbose = 1
+            verbose = verbose,
+            **kwargs
             )
         # print
         # summary(self, input_size=(1, *self._input_shape))
@@ -204,7 +206,6 @@ class YOLOv3(nn.Module):
         Output block for YOLOv3.
         parameters:
             in_channels: number of input channels
-            out_channels: number of output channels
             num_classes: number of classes
         """
 
@@ -232,14 +233,146 @@ class YOLOv3(nn.Module):
                 .permute(0, 1, 3, 4, 2)
             )
 
-class TinyYOLOv3:
+class TinyYOLOv3(nn.Module):
     """
     Tiny YOLOv3 architecture.
     """
 
+    def __init__(
+        self,
+        input_shape: tuple,
+        num_classes: int,
+        initial_filters: int=16,
+        base_output_scale: tuple=(13, 13),
+        anchors: list = None,
+    ):
+        """
+        parameters:
+            input_shape: shape of the input image
+            num_classes: number of classes
+        """
+        super().__init__()
+        self._input_shape = input_shape
+        self._num_classes = num_classes
+        self._initial_filters = initial_filters
+        self._base_output_scale = base_output_scale
+        self._anchors = anchors
+
+        self.layers = self._create_model()
+
+    def forward(self, x):
+        outputs = []
+
+        for i, layer in enumerate(self.layers):
+            if isinstance(layer, self._OutputBlock):
+                outputs.append(layer(x))
+                continue
+
+            x = layer(x)
+            if i == 4:
+                route_1 = x
+            elif isinstance(layer, nn.Upsample):
+                x = torch.cat((x, route_1), dim=1)
+
+        return outputs
+
+    def summary(self, verbose: int=1, **kwargs):
+        """
+        Prints the summary of the model.
+        """
+        summary(
+            self,
+            input_size=self._input_shape,
+            batch_dim=0,
+            col_names = ("input_size", "output_size", "num_params", "kernel_size"),
+            verbose = verbose,
+            **kwargs
+            )
+        # print
+        # summary(self, input_size=(1, *self._input_shape))
+
+    def _create_model(self):
+
+        FILTERS = self._initial_filters
+        layers = nn.ModuleList([
+        # Darknet-19 starts
+            self._ConvMaxPollBlock(in_channels=3, out_channels=FILTERS),
+            self._ConvMaxPollBlock(in_channels=FILTERS, out_channels=FILTERS*2),
+            self._ConvMaxPollBlock(in_channels=FILTERS*2, out_channels=FILTERS*4),
+            self._ConvMaxPollBlock(in_channels=FILTERS*4, out_channels=FILTERS*8),
+            self._ConvMaxPollBlock(in_channels=FILTERS*8, out_channels=FILTERS*16), # Route 1
+            self._ConvMaxPollBlock(in_channels=FILTERS*16, out_channels=FILTERS*32, max_poll_stride="same"),
+            ConvBlock(in_channels=FILTERS*32, out_channels=FILTERS*64, kernel_size=3),
+        # Darknet-19 ends
+            ConvBlock(in_channels=FILTERS*64, out_channels=FILTERS*16, kernel_size=1),
+            self._OutputBlock(in_channels=FILTERS*16, channels=FILTERS*32, num_classes=self._num_classes),
+
+            ConvBlock(in_channels=FILTERS*16, out_channels=FILTERS*8, kernel_size=1),
+            nn.Upsample(scale_factor=2), # Route 1
+            # Join 1
+            self._OutputBlock(in_channels=FILTERS*24, channels=FILTERS*16, num_classes=self._num_classes),
+        ])
+
+        return layers
+    class _ConvMaxPollBlock(nn.Module):
+        """
+        Convolutional + MaxPool block for Tiny YOLOv3.
+        parameters:
+            in_channels: number of input channels
+            out_channels: number of output channels
+            stride: stride for MaxPool
+        """
+        def __init__(
+            self,
+            in_channels: int,
+            out_channels: int,
+            max_poll_stride: int=2
+        ):
+            super().__init__()
+            self._layers = nn.Sequential(
+                ConvBlock(in_channels, out_channels, 3),
+                nn.MaxPool2d(kernel_size=2, stride=max_poll_stride)
+            )
+
+        def forward(self, x):
+            return self._layers(x)
+
+    class _OutputBlock(nn.Module):
+        """
+        Output block for Tiny YOLOv3.
+        parameters:
+            in_channels: number of input channels
+            channels: number of channels in the output of first ConvBlock
+            num_classes: number of classes
+        """
+        def __init__(
+            self,
+            in_channels: int,
+            channels: int,
+            num_classes: int,
+        ):
+            super().__init__()
+            self._layers = nn.Sequential(
+                ConvBlock(in_channels, channels, 3),
+                ConvBlock(
+                    channels,
+                    (5 + num_classes)*3,
+                    kernel_size=1,
+                    normalize=False
+                )
+            )
+            self.num_classes = num_classes
+
+        def forward(self, x):
+            return (
+                self._layers(x)
+                .reshape(x.shape[0], 3, 5 + self.num_classes, x.shape[2], x.shape[3])
+                .permute(0, 1, 3, 4, 2)
+            )
+
 if __name__ == "__main__":
-    num_classes = 20
-    # class_labels = CLASS_LABELS
+    num_classes = NUM_CLASSES
+    class_labels = CLASS_LABELS
     IMAGE_SIZE = 416
     initial_filters = 32
 
@@ -251,8 +384,13 @@ if __name__ == "__main__":
     assert out[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, 5 + num_classes)
     assert out[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, 5 + num_classes)
     assert out[2].shape == (2, 3, IMAGE_SIZE//8, IMAGE_SIZE//8, 5 + num_classes)
+    print("YOLOv3 Test passed.\n"+"-"*15)
+    # model.summary(verbose=1)
+    print("\n")
 
-    print("Test passed.\n"+"-"*15)
+    model = TinyYOLOv3(input_shape, num_classes, initial_filters)
+    out = model(x)
 
-    model.summary()
-
+    assert out[0].shape == (2, 3, IMAGE_SIZE//32, IMAGE_SIZE//32, 5 + num_classes)
+    assert out[1].shape == (2, 3, IMAGE_SIZE//16, IMAGE_SIZE//16, 5 + num_classes)
+    print("TinyYOLOv3 Test passed.\n"+"-"*15)
