4b5c736b5e56aa8c09439607cf65bcdb9f42dc6e
aka-vm <vineetmahajan2000@gmail.com>
2023-05-30 12:57:34 +0530

    indent correctet and test ran

--- yolov3_torch/dataset.py
+++ yolov3_torch/dataset.py
@@ -13,7 +13,8 @@ from PIL import Image, ImageFile
 import config
 from utils import (
     cells_to_bboxes,
-    iou,
+    iou_wh,
+    scale_anchor_boxes,
     # non_max_suppression as nms,
     # plot_image
 )
@@ -45,52 +46,53 @@ class YOLODataset(Dataset):
         self.image_size = image_size
         self.ignore_iou_thresh = ignore_iou_thresh
 
-        def __len__(self):
-            return len(self.annotations)
+    def __len__(self):
+        return len(self.annotations)
 
-        def __getitem__(self, index: int):
-            label_path = label_dir / self.annotations.iloc[index, 1]
-            bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), -1, axis=1).tolist() # (x, y, w, h, c)
-            img_path = img_dir / self.annotations.iloc[index, 0]
-            image = np.array(Image.open(img_path).convert("RGB"))
+    def __getitem__(self, index: int):
+        label_path = self.label_dir / self.annotations.iloc[index, 1]
+        # (x, y, w, h, c)
+        bboxes = np.roll(np.loadtxt(fname=label_path, delimiter=" ", ndmin=2), -1, axis=1).tolist()
+        img_path = self.img_dir / self.annotations.iloc[index, 0]
+        image = np.array(Image.open(img_path).convert("RGB"))
 
-            if self.transform:
-                augmentations = self.transform(image=image, bboxes=bboxes)
-                image = augmentations["image"]
-                bboxes = augmentations["bboxes"]
+        if self.transform:
+            augmentations = self.transform(image=image, bboxes=bboxes)
+            image = augmentations["image"]
+            bboxes = augmentations["bboxes"]
 
-            # taking 3 scaled predictions from the YOLOv3 model #! Modify later to make it general
-            # targets shape - [tensor(3, s, s, 6),];
-            targets = [torch.zeros((self.num_anchors_per_scale, s, s, 6)) for s in self.scales]
-            #
-            for box in bboxes:
-                iou_anchors = iou(torch.tensor(box[2:4]), self.anchors)
-                anchors_rank = iou_anchors.argsort(descending=True, dim=0)
-                x, y, w, h, class_label = box   # x, y, w, h are relative to the image size
-                scale_flag = [False, False, False]
-                for i_anchor in anchors_rank:
-                    i_scale = i_anchor // self.num_anchors_per_scale
-                    anchor_on_scale = i_anchor % self.num_anchors_per_scale
-                    # w_cell, h_cell = self.image_size / self.S[i_scale]
-                    scale = self.scales[i_scale]
-                    i, j = int(scale * y), int(scale * x)
-                    target_anchor_taken = targets[i_scale][anchor_on_scale, i, j, 0]
-                    if not target_anchor_taken and not scale_flag[i_scale]:
-                        targets[i_scale][anchor_on_scale, i, j, 0] = 1
+        # taking 3 scaled predictions from the YOLOv3 model #! Modify later to make it general
+        # targets shape - [tensor(3, s, s, 6),];
+        targets = [torch.zeros((self.num_anchors_per_scale, s, s, 6)) for s in self.scales]
+        #
+        for box in bboxes:
+            iou_anchors = iou_wh(torch.tensor(box[2:4]), self.anchors)
+            anchors_rank = iou_anchors.argsort(descending=True, dim=0)
+            x, y, w, h, class_label = box   # x, y, w, h are relative to the image size
+            scale_flag = [False, False, False]
+            for i_anchor in anchors_rank:
+                i_scale = int(i_anchor / self.num_anchors_per_scale)
+                anchor_on_scale = i_anchor % self.num_anchors_per_scale
+                # w_cell, h_cell = self.image_size / self.S[i_scale]
+                scale = self.scales[i_scale]
+                i, j = int(scale * y), int(scale * x)
+                target_anchor_taken = targets[i_scale][anchor_on_scale, i, j, 0]
+                if not target_anchor_taken and not scale_flag[i_scale]:
+                    targets[i_scale][anchor_on_scale, i, j, 0] = 1
 
-                        x_cell, y_cell = scale * x - j, scale * y - i
-                        w_cell = w * scale
-                        h_cell = h * scale
-                        box_coordinates = torch.tensor([x_cell, y_cell, w_cell, h_cell])
+                    x_cell, y_cell = scale * x - j, scale * y - i
+                    w_cell = w * scale
+                    h_cell = h * scale
+                    box_coordinates = torch.tensor([x_cell, y_cell, w_cell, h_cell])
 
-                        targets[i_scale][anchor_on_scale, i, j, 1:5] = box_coordinates
-                        targets[i_scale][anchor_on_scale, i, j, 5] = int(class_label)
-                        scale_flag[i_scale] = True
+                    targets[i_scale][anchor_on_scale, i, j, 1:5] = box_coordinates
+                    targets[i_scale][anchor_on_scale, i, j, 5] = int(class_label)
+                    scale_flag[i_scale] = True
 
-                    elif not target_anchor_taken and iou_anchors[i_anchor] > self.ignore_iou_thresh:
-                        targets[i_scale][anchor_on_scale, i, j, 0] = -1 # will be ignored during loss calculation
+                elif not target_anchor_taken and iou_anchors[i_anchor] > self.ignore_iou_thresh:
+                    targets[i_scale][anchor_on_scale, i, j, 0] = -1 # will be ignored during loss calculation
 
-            return image, tuple(targets)
+        return image, tuple(targets)
 
 def test():
     anchors = config.ANCHORS
@@ -109,7 +111,14 @@ def test():
         anchors=anchors,
         transform=transform
     )
-    # scales = (13, 26, 52)
+    scales = (13, 26, 52)
+    scaled_anchors = scale_anchor_boxes(anchors, scales)
+    data_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+    for img, y in data_loader:
+        boxes = []
+
+        for i in range(scaled_anchors[0].shape[1]):
+            pass
 
 
 if __name__ == "__main__":
