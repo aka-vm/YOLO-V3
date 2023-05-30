4134803f617fe09b54d5d2c2e1ff52759a79467a
aka-vm <vineetmahajan2000@gmail.com>
2023-05-30 12:58:32 +0530

    iou_hw and iou; tested

--- yolov3_torch/utils.py
+++ yolov3_torch/utils.py
@@ -3,7 +3,7 @@ from torch import nn
 
 import numpy as np
 
-def anchor_box_convert(anchor_boxes: list[list[tuple[float, float]]], output_frame_size=((13, 13), (26, 26), (52, 52))):
+def scale_anchor_boxes(anchor_boxes: list[list[tuple[float, float]]], output_scales=(13, 26, 52)):
     """
     Process anchor boxes to be relative to the output frame size.
 
@@ -12,30 +12,81 @@ def anchor_box_convert(anchor_boxes: list[list[tuple[float, float]]], output_fra
             Matrix of tuples of floats(0-1) representing the anchor box size
             ratio w.r.t. the original input image size.
 
-        output_frame_size: list of tuples of ints representing the output
+        output_scales: list of ints or list of tuple(scale_h, scale_w) representing the output
         frame size, depending on the YOLO layer.
 
     Returns:
-        Matrix of tuples of floats(0-output_frame_size[i][j]) representing
+        Matrix of tuples of floats(0-output_scale_size) representing
         the anchor box size w.r.t. the output frame size.
     """
-    anchor_boxes = np.array(anchor_boxes)
+    anchor_boxes = torch.tensor(anchor_boxes)
+    output_scales = torch.tensor(output_scales)
+    if output_scales.ndim == 1:
+        output_scales = output_scales.unsqueeze(1).repeat(1, 2)
+    output_scales = output_scales.unsqueeze(1).repeat(1, anchor_boxes.shape[1], 1)
 
-    output = []
-    for i, frame_size in enumerate(output_frame_size):
-        frame_size = frame_size
-        anchor_box = anchor_boxes[i]
-        anchor_box = anchor_box * frame_size
-        output.append(anchor_box)
-
-    return np.array(output)
+    return anchor_boxes * output_scales
 
 
-def iou(boxes1_dims, boxes2_dims):
-    # intersection = np.minimum(box1_dims, box2_dims).prod(axis=-1)
-    intersection = torch.min(boxes1_dims, boxes2_dims).prod(axis=-1)
-    # union = box1_dims.prod(axis=-1) + box2_dims.prod(axis=-1) - intersection
-    union = boxes1_dims.prod(axis=-1) + boxes2_dims.prod(axis=-1) - intersection
+def iou_wh(boxes1_wh, boxes2_wh):
+    # Used to choose the best anchor box for a given bounding box
+    intersection = torch.min(boxes1_wh, boxes2_wh).prod(axis=-1)
+    union = boxes1_wh.prod(axis=-1) + boxes2_wh.prod(axis=-1) - intersection
+
+    return intersection / union
+
+def iou_boxes(boxes1, boxes2, box_format="midpoint"):
+    # used to check how good pred box is wrt target box and calculate the loss
+    # box_format: midpoint/corners
+    """
+    if box_format == "midpoint":
+        boxes_n -> [x, y, w, h]
+        bnx1 -> x - w/2
+        bny1 -> y - h/2
+        bnx2 -> x + w/2
+        bny2 -> y + h/2
+
+    elif box_format == "corners":
+        boxes_n -> [bnx1, bny1, bnx2, bny2]
+
+    x1 -> max(b1x1, b2x1)
+    x2 -> min(b1x2, b2x2)
+    y1 -> max(b1y1, b2y1)
+    y2 -> min(b1y2, b2y2)
+
+    Intersection -> Area(x1, y1, x2, y2)
+    Union -> Area(b1) + Area(b2) - Intersection
+
+    IOU -> Intersection / Union
+    """
+    if box_format == "midpoint":
+        b1x1 = boxes1[..., 0:1] - boxes1[..., 2:3] / 2
+        b1y1 = boxes1[..., 1:2] - boxes1[..., 3:4] / 2
+        b1x2 = boxes1[..., 0:1] + boxes1[..., 2:3] / 2
+        b1y2 = boxes1[..., 1:2] + boxes1[..., 3:4] / 2
+        b2x1 = boxes2[..., 0:1] - boxes2[..., 2:3] / 2
+        b2y1 = boxes2[..., 1:2] - boxes2[..., 3:4] / 2
+        b2x2 = boxes2[..., 0:1] + boxes2[..., 2:3] / 2
+        b2y2 = boxes2[..., 1:2] + boxes2[..., 3:4] / 2
+    elif box_format == "corners":
+        b1x1 = boxes1[..., 0:1]
+        b1y1 = boxes1[..., 1:2]
+        b1x2 = boxes1[..., 2:3]
+        b1y2 = boxes1[..., 3:4]
+        b2x1 = boxes2[..., 0:1]
+        b2y1 = boxes2[..., 1:2]
+        b2x2 = boxes2[..., 2:3]
+        b2y2 = boxes2[..., 3:4]
+
+    x1 = torch.max(b1x1, b2x1)
+    x2 = torch.min(b1x2, b2x2)
+    y1 = torch.max(b1y1, b2y1)
+    y2 = torch.min(b1y2, b2y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    union = abs((b1x2 - b1x1) * (b1y2 - b1y1)) \
+          + abs((b2x2 - b2x1) * (b2y2 - b2y1)) \
+          - intersection
 
     return intersection / union
 
